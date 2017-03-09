6ba86db528e9bbf2a7596daf1aecf9ea90687b43
dglv <ddem0025@gmail.com>
2017-03-09 20:02:22 +0300

    Renamed some files

---
+++ 0003_data_types/js/types-output.js
@@ -0,0 +1,20 @@
+// "undefined"
+console.log('typeof undefined: ', typeof undefined);
+
+// "number"
+console.log('typeof 0: ', typeof 0);
+
+typeof true // "boolean"
+console.log('typeof true: ', typeof true);
+
+typeof "foo" // "string"
+console.log('typeof \'foo\': ', typeof 'foo');
+
+// "object"
+console.log('typeof {}: ', typeof {});
+
+//typeof null == "object" but should be null
+console.log('typeof null: ', typeof null);
+
+// "function"
+console.log('typeof function(){}: ', typeof function(){});
